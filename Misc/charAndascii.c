2a566b3007e0a8fa088e05d01b6316b89d9f1ac9
Antony Herald I <31497965+AntonyHerald@users.noreply.github.com>
2021-07-03 23:52:08 +0530

    Generate ascii, hexa, octal

---
+++ Misc/charAndascii.c
@@ -0,0 +1,21 @@
+
+#include <stdio.h>
+
+int main()
+{
+  char ch1, ch2, ch3;
+  ch1 = 65; 
+  ch2 = 'b';
+  ch3 = '3';
+
+  	printf("Ch1 = %c \n",ch1);
+  	printf("Ch1 = %c \n",ch2);
+  	printf("Ch1 = %c \n",ch3);
+
+  	printf("Ch1 = %d \n",ch1);
+  	printf("Ch1 = %d \n",ch2);
+  	printf("Ch1 = %d \n",ch3);
+
+
+	return 0;
+}
